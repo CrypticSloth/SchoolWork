391816227cd1ba51f9a16042e91579f7044741e1
CrypticSl0th <sorensen.erik48@gmail.com>
2019-02-07 17:32:44 -0500

    Update after session with Dr. Hu

--- QuantumResearch/CV_QuantumNeuralNetworks/optimization_Wei.py
+++ QuantumResearch/CV_QuantumNeuralNetworks/optimization_Wei.py
@@ -7,29 +7,30 @@ import tensorflow as tf
 # You can test it with y = 0.5, 0.7, 0.9
 # y = 0.5
 
-x = [[0.1], [0.4], [0.6], [0.8]]
-y = [[0.2], [0.5], [0.7], [0.9]]
+x_data = [[0.1], [0.4], [0.6], [0.8]]
+y_data = [[0.2], [0.5], [0.7], [0.9]]
 
-# import numpy as np
-# x = np.array([[0.2, 0.4], [0.6, 0.8], [0.4, 0.2], [0.8, 0.6]])
-# x[:,0]
-# y = [[0.1,0.2],[0.4,0.5],[0.6,0.7],[0.8,0.9]]
-
-# y = beta * x + alpha
+x = tf.placeholder(tf.float32,shape=[1])
+y = tf.placeholder(tf.float32,shape=[1])
 
 eng, q = sf.Engine(1)
 
 alpha = tf.Variable(0.1)
 beta = tf.Variable(0.1)
+beta1 = tf.Variable(0.1)
+alpha1 = tf.Variable(0.1)
+
 with eng:
-    Dgate(np.array(x))  | q[0]
-    Dgate(alpha)   | q[0]
-    Sgate(beta)  | q[0]
+    Dgate(x[0])         | q[0]
+    Dgate(alpha)        | q[0]
+    Sgate(beta,beta1)   | q[0]
+    Kgate(alpha1)       | q[0]
+
 state = eng.run('tf', cutoff_dim=7, eval=False)
 
 # loss is probability for the Fock state n=0
 prob = state.fock_prob([0])
-loss = tf.square(prob - y)
+loss = tf.square([prob] - y)
 
 # Set up optimization
 optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.1)
@@ -40,9 +41,13 @@ sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
 # Carry out optimization
-for step in range(50):
-    prob_val, _ = sess.run([prob, minimize_op])
-    print("Value at step {}: {}".format(step, prob_val))
+for step in range(500):
+    for i in range(4):
+        prob_val, _ = sess.run([prob, minimize_op],feed_dict={x:x_data[i],y:y_data[i]})
+
+for i in range(4):
+    prob_val, _ = sess.run([prob,minimize_op],feed_dict={x:x_data[i],y:y_data[i]})
+    print(prob_val,y_data[i])
 
 
 # Your exercise is to use this code to build a quantum model to fit this linear dataset:
@@ -52,84 +57,84 @@ for step in range(50):
 # then write a classical Tensorflow program to fit this dataset.
 
 # Classical Tensorflow to approximate y
-import tensorflow as tf
-
-x = tf.Variable(1.0)
-y = 0.9
-
-loss = tf.square(x - y)
-
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.1)
-minimize_op = optimizer.minimize(loss)
-
-sess = tf.Session()
-sess.run(tf.global_variables_initializer())
-
-for step in range(50):
-    prob_val, _ = sess.run([x,minimize_op])
-    print("Value at step {}: {}".format(step,prob_val))
-
-# Tensorflow program to fit the data
-import tensorflow as tf
-
-# X = [[0.1], [0.4], [0.6], [0.8]]
-# Y = [[0.2], [0.5], [0.7], [0.9]]
-
-x = tf.Variable([[0.1], [0.4], [0.6], [0.8]])
-y = tf.Variable([[0.2], [0.5], [0.7], [0.9]])
-
-loss = tf.square(x - y)
-
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.2)
-minimize_op = optimizer.minimize(loss)
-
-sess = tf.Session()
-sess.run(tf.global_variables_initializer())
-
-for step in range(50):
-    prob_val, _ = sess.run([x,minimize_op])
-    print("Value at step {}: {}".format(step,prob_val))
-
-
-# x_ = tf.placeholder(tf.float32, shape=[4,1], name = 'x-input')
-# y_ = tf.placeholder(tf.float32, shape=[1], name = 'y-input')
+# import tensorflow as tf
 #
-# Theta1 = tf.Variable(tf.random_uniform([2,2], -1, 1), name = "Theta1")
-# Theta2 = tf.Variable(tf.random_uniform([1], -1, 1), name = "Theta2")
+# x = tf.Variable(1.0)
+# y = 0.9
 #
-# Bias1 = tf.Variable(tf.zeros([2]), name = "Bias1")
-# Bias2 = tf.Variable(tf.zeros([1]), name = "Bias2")
+# loss = tf.square(x - y)
 #
-
+# optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.1)
+# minimize_op = optimizer.minimize(loss)
 #
-# with tf.name_scope("layer3") as scope:
-# 	Hypothesis = tf.sigmoid(tf.matmul(A2, Theta2) + Bias2)
-#
-# with tf.name_scope("cost") as scope:
-# 	cost = tf.reduce_mean(( (y_ * tf.log(Hypothesis)) +
-# 		((1 - y_) * tf.log(1.0 - Hypothesis)) ) * -1)
-#
-# with tf.name_scope("train") as scope:
-# 	train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cost)
-#
-# x = [[0.1], [0.4], [0.6], [0.8]]
-# y = [[0.2], [0.5], [0.7], [0.9]]
-#
-# init = tf.global_variables_initializer()
 # sess = tf.Session()
+# sess.run(tf.global_variables_initializer())
 #
-# writer = tf.summary.FileWriter("./logs/xor_logs", sess.graph)
+# for step in range(50):
+#     prob_val, _ = sess.run([x,minimize_op])
+# # print("Value at step {}: {}".format(step,prob_val))
 #
-# sess.run(init)
+# # Tensorflow program to fit the data
+# import tensorflow as tf
 #
-# t_start = time.clock()
-# for i in range(100000):
-# 	sess.run(train_step, feed_dict={x_: XOR_X, y_: XOR_Y})
-# 	if i % 1000 == 0:
-# 		print('Epoch ', i)
-# 		print('Hypothesis ', sess.run(Hypothesis, feed_dict={x_: XOR_X, y_: XOR_Y}))
-# 		print('Theta1 ', sess.run(Theta1))
-# 		print('Bias1 ', sess.run(Bias1))
-# 		print('Theta2 ', sess.run(Theta2))
-# 		print('Bias2 ', sess.run(Bias2))
-# print('cost ', sess.run(cost, feed_dict={x_: XOR_X, y_: XOR_Y}))
+# # X = [[0.1], [0.4], [0.6], [0.8]]
+# # Y = [[0.2], [0.5], [0.7], [0.9]]
+#
+# x = tf.Variable([[0.1], [0.4], [0.6], [0.8]])
+# y = tf.Variable([[0.2], [0.5], [0.7], [0.9]])
+#
+# loss = tf.square(x - y)
+#
+# optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.2)
+# minimize_op = optimizer.minimize(loss)
+#
+# sess = tf.Session()
+# sess.run(tf.global_variables_initializer())
+#
+# for step in range(50):
+#     prob_val, _ = sess.run([x,minimize_op])
+#     # print("Value at step {}: {}".format(step,prob_val))
+#
+#
+# # x_ = tf.placeholder(tf.float32, shape=[4,1], name = 'x-input')
+# # y_ = tf.placeholder(tf.float32, shape=[1], name = 'y-input')
+# #
+# # Theta1 = tf.Variable(tf.random_uniform([2,2], -1, 1), name = "Theta1")
+# # Theta2 = tf.Variable(tf.random_uniform([1], -1, 1), name = "Theta2")
+# #
+# # Bias1 = tf.Variable(tf.zeros([2]), name = "Bias1")
+# # Bias2 = tf.Variable(tf.zeros([1]), name = "Bias2")
+# #
+#
+# #
+# # with tf.name_scope("layer3") as scope:
+# # 	Hypothesis = tf.sigmoid(tf.matmul(A2, Theta2) + Bias2)
+# #
+# # with tf.name_scope("cost") as scope:
+# # 	cost = tf.reduce_mean(( (y_ * tf.log(Hypothesis)) +
+# # 		((1 - y_) * tf.log(1.0 - Hypothesis)) ) * -1)
+# #
+# # with tf.name_scope("train") as scope:
+# # 	train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cost)
+# #
+# # x = [[0.1], [0.4], [0.6], [0.8]]
+# # y = [[0.2], [0.5], [0.7], [0.9]]
+# #
+# # init = tf.global_variables_initializer()
+# # sess = tf.Session()
+# #
+# # writer = tf.summary.FileWriter("./logs/xor_logs", sess.graph)
+# #
+# # sess.run(init)
+# #
+# # t_start = time.clock()
+# # for i in range(100000):
+# # 	sess.run(train_step, feed_dict={x_: XOR_X, y_: XOR_Y})
+# # 	if i % 1000 == 0:
+# # 		print('Epoch ', i)
+# # 		print('Hypothesis ', sess.run(Hypothesis, feed_dict={x_: XOR_X, y_: XOR_Y}))
+# # 		print('Theta1 ', sess.run(Theta1))
+# # 		print('Bias1 ', sess.run(Bias1))
+# # 		print('Theta2 ', sess.run(Theta2))
+# # 		print('Bias2 ', sess.run(Bias2))
+# # print('cost ', sess.run(cost, feed_dict={x_: XOR_X, y_: XOR_Y}))
