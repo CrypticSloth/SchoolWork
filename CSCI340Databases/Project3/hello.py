f845a4d81c0255901882189fe5790a8c78949def
CrypticSl0th <sorensen.erik48@gmail.com>
2019-03-20 12:38:29 -0400

    postgresql connection tutorial

--- CSCI340Databases/Project3/hello.py
+++ CSCI340Databases/Project3/hello.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 # Good tutorial here: https://code.tutsplus.com/tutorials/creating-a-web-app-from-scratch-using-python-flask-and-mysql--cms-22972
-
+# To connect to postgresql server: https://realpython.com/flask-by-example-part-2-postgres-sqlalchemy-and-alembic/
 app = Flask(__name__)
 
 @app.route('/')
