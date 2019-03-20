d9d77211e07064610896890940c1477139e82ab9
CrypticSl0th <sorensen.erik48@gmail.com>
2019-03-20 12:25:56 -0400

    Added tutorial website comment

--- CSCI340Databases/Project3/hello.py
+++ CSCI340Databases/Project3/hello.py
@@ -1,5 +1,7 @@
 from flask import Flask
 
+# Good tutorial here: https://code.tutsplus.com/tutorials/creating-a-web-app-from-scratch-using-python-flask-and-mysql--cms-22972
+
 app = Flask(__name__)
 
 @app.route('/')
